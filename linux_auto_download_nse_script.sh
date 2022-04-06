fb216a58c6451b1d54e2d07f7542c4cb8b062acc
shani-d <46711777+shani-d@users.noreply.github.com>
2022-04-06 15:08:03 +0300

    Update linux_auto_download_nse_script.sh

--- linux_auto_download_nse_script.sh
+++ linux_auto_download_nse_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://github.com/VicariusInc/vicarius-nmap/raw/main/cve.zip -O cve.zip
-wget https://github.com/VicariusInc/vicarius-nmap/raw/main/vicarius-vulnerability-scan.nse -O vicarius-vulnerability-scan.nse
+wget https://github.com/VicariusInc/vicarius-nmap/releases/download/Latest/cve.zip -O cve.zip
+wget https://github.com/VicariusInc/vicarius-nmap/releases/download/Latest/vicarius-vulnerability-scan.nse -O vicarius-vulnerability-scan.nse
 
 sudo cp vicarius-vulnerability-scan.nse /usr/share/nmap/scripts/vicarius-vulnerability-scan.nse
 sudo mkdir /usr/share/nmap/scripts/vicarius-vulnerability-scan/
